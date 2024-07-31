c668ef6e90f2f83d32f3f9166b518a7a06c16426
HM-bcard <111281060+HM-bcard@users.noreply.github.com>
2024-08-01 00:02:14 +0200

    Create day3.py

---
+++ day3.py
@@ -0,0 +1,15 @@
+wassuuuup = input('Whassuuuuup?\n')
+
+#wassuuuup()
+
+print(wassuuuup)
+
+def wassuuuuup():
+    if wassuuuup == 'Whassuuuuup?':
+        print('WHASSUUUUUP?')
+    else:
+        print("Hello, WHo is this?")
+
+wassuuuuup()
+
+ 
