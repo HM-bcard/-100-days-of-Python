d93c16fae8cee294a931d7824b83c52e0c9abeac
HM-bcard <111281060+HM-bcard@users.noreply.github.com>
2024-08-10 02:03:19 +0200

    Update day6.py

--- day6.py
+++ day6.py
@@ -1,1 +1,7 @@
 
+def circle():
+    radius = float(input('Please input the radius of the circle below: \n'))
+    area = (3.14159 * radius * radius)
+    print(f"The area of this circle is {area:.2f}")
+
+
