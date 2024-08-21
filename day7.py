f4ab1a81d3a92f8b88a6b56d3f4ce4b1f60fad68
HM-bcard <111281060+HM-bcard@users.noreply.github.com>
2024-08-21 04:19:02 +0200

    Update day7.py

--- day7.py
+++ day7.py
@@ -1,1 +1,6 @@
 
+def temp_converter():
+  temp = int(input("please input the temperature in Celsius here:")
+  temp_fahrenheit = (temp*1.8) + 32
+  print(f"the temperature in fahrenheit is {temp_fahrenheit:.2f}.")
+  
